deadaae377d85cb52c236af5a07b3e5b546eb0ab
filipemedeiross <filipemedeiros2013@gmail.com>
2024-01-11 10:18:28 -0300

    Final version of the main screen

--- flappy_bean/flappybean.py
+++ flappy_bean/flappybean.py
@@ -7,39 +7,34 @@ from webbrowser import open
 
 
 class FlappyBean:
-    BG_PATH = 'flappy_bean/media/bg.png'
+    BG_PATH       = GAME_BG_PATH
+    FLAPPY_PATH   = GAME_FLAPPY_PATH
+    FINGER_PATH   = GAME_FINGER_PATH
+    TAPLEFT_PATH  = GAME_TAPLEFT_PATH
+    TAPRIGHT_PATH = GAME_TAPRIGHT_PATH
+    GETREADY_PATH = GAME_GETREADY_PATH
+    SHARE_PATH    = GAME_SHARE_PATH
+    GAMEOVER_PATH = GAME_GAMEOVER_PATH
+    MENU_PATH     = GAME_MENU_PATH
+    OK_PATH       = GAME_OK_PATH
+    DIGITS_PATH   = GAME_DIGITS_PATH
+    THEME_PATH    = GAME_THEME_PATH
+    EFFCT_PATH    = GAME_EFFCT_PATH
 
-    FLAPPY_PATH   = 'flappy_bean/media/flappy.png'
-    FINGER_PATH   = 'flappy_bean/media/finger.png'
-    TAPLEFT_PATH  = 'flappy_bean/media/tap_left.png'
-    TAPRIGHT_PATH = 'flappy_bean/media/tap_right.png'
-    GETREADY_PATH = 'flappy_bean/media/get_ready.png'
-    SHARE_PATH    = 'flappy_bean/media/share.png'
-
-    GAMEOVER_PATH = 'flappy_bean/media/game_over.png'
-    MENU_PATH     = 'flappy_bean/media/menu.png'
-    OK_PATH       = 'flappy_bean/media/ok.png'
-    DIGITS_PATH = [f'flappy_bean/media/{i}.png' for i in range(10)]
-
-    THEME_PATH = 'flappy_bean/media/epic_at_the_jungle.mp3'
-    EFFCT_PATH = 'flappy_bean/media/gameover.mp3'
     def __init__(self):
         pygame.init()
 
         # Screen is initialized with init_game method
         self.screen = None
 
-        # Init the clock
+        # Init the clock and mixer
         self.clock = pygame.time.Clock()
         
-        # Init the mixer
         self.music_game = pygame.mixer.Sound(self.THEME_PATH)
         self.music_lose = pygame.mixer.Sound(self.EFFCT_PATH)
 
         self.channel_music = pygame.mixer.Channel(0)
         self.channel_effct = pygame.mixer.Channel(1)
-
-        self.music_game.set_volume(0.5)
 
         # Loading images used in the game
         self.background = pygame.transform.scale(pygame.image.load(self.BG_PATH), SCREEN_SIZE)
@@ -56,13 +51,13 @@ class FlappyBean:
         self.tap_right_rect = self.tap_right.get_rect(topleft=(self.finger_rect.right + SPACING, FINGER_TOP))
 
         self.get_ready = pygame.transform.scale(pygame.image.load(self.GETREADY_PATH), GETREADY_SIZE)
-        self.get_ready_rect = self.get_ready.get_rect(midtop=(SCREEN_MIDW, self.finger_rect.bottom + 3 * SPACING))
+        self.get_ready_rect = self.get_ready.get_rect(midtop=(SCREEN_MIDW, self.finger_rect.bottom + SPACING_3))
 
         self.share = pygame.transform.scale(pygame.image.load(self.SHARE_PATH), SHARE_SIZE)
-        self.share_rect = self.share.get_rect(midtop=(SCREEN_MIDW, self.get_ready_rect.bottom + 2 * SPACING))
+        self.share_rect = self.share.get_rect(midtop=(SCREEN_MIDW, self.get_ready_rect.bottom + SPACING_2))
 
         self.game_over = pygame.transform.scale(pygame.image.load(self.GAMEOVER_PATH), GAMEOVER_SIZE)
-        self.game_over_rect = self.game_over.get_rect(center=(SCREEN_MIDW, GAMEOVER_TOP))
+        self.game_over_rect = self.game_over.get_rect(midtop=(SCREEN_MIDW, GAMEOVER_TOP))
 
         self.menu = pygame.transform.scale(pygame.image.load(self.MENU_PATH), MENU_SIZE)
         self.menu_rect = self.menu.get_rect(topleft=(self.game_over_rect.left, self.game_over_rect.bottom + SPACING))
@@ -70,17 +65,16 @@ class FlappyBean:
         self.ok = pygame.transform.scale(pygame.image.load(self.OK_PATH), OK_SIZE)
         self.ok_rect = self.ok.get_rect(topright=(self.game_over_rect.right, self.game_over_rect.bottom + SPACING))
 
-        self.digits = [pygame.transform.scale(pygame.image.load(path), DIGITS_SIZE) for path in self.DIGITS_PATH]
+        self.digits = [pygame.transform.scale(pygame.image.load(p), DIGITS_SIZE) for p in self.DIGITS_PATH]
         self.first_digit_rect  = self.digits[0].get_rect(topleft=(SCORE_LEFT, SCORE_TOP))
         self.second_digit_rect = self.digits[0].get_rect(topleft=(SCORE_LEFT + DIGITS_SIDE, SCORE_TOP))
         self.third_digit_rect  = self.digits[0].get_rect(topleft=(SCORE_LEFT + 2 * DIGITS_SIDE, SCORE_TOP))
 
         # Initializing game objects
-        self.bean  = Bean(0, 0)
+        self.bean  = Bean(BEAN_MSCW, BEAN_MSCH)
+        self.base  = Base(SCREEN_HGHT - BASE_HGHT)
         self.pipes = []
-        self.base  = Base(BASE_Y)
 
-    # Method that start the game
     def init_game(self):
         # Creating a display Surface
         self.screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -91,22 +85,9 @@ class FlappyBean:
             self.play()
 
     def main_screen(self):
-        # Preparing the main screen
-        self.screen.blit(self.background, (0, 0))
-
-        self.screen.blit(self.flappy, self.flappy_rect)
-        self.screen.blit(self.finger, self.finger_rect)
-        self.screen.blit(self.tap_left, self.tap_left_rect)
-        self.screen.blit(self.tap_right, self.tap_right_rect)
-        self.screen.blit(self.get_ready, self.get_ready_rect)
-        self.screen.blit(self.share, self.share_rect)
-
-        pygame.display.flip()  # displaying the screen
-
-        self.bean.topleft = BEAN_MSCW, BEAN_MSCH
-
-        if not self.channel_music.get_busy():
-            self.channel_music.play(self.music_game, -1)
+        self.init_main_screen()
+        self.display_main_screen()
+        self.play_theme()
 
         while True:
             self.clock.tick(FRAMERATE_MS)
@@ -115,23 +96,16 @@ class FlappyBean:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit(0)  # leaving the game
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        return
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.finger_rect.collidepoint(event.pos):
                         return
                     if self.share_rect.collidepoint(event.pos):
                         open('https://github.com/filipemedeiross/', new=2)
 
-            self.screen.blit(self.background,
-                             self.bean.topleft,
-                             area=self.bean.rect)
-            pygame.display.update(self.bean.rect)
-            
-            self.bean.move()
-            if self.bean.y >= SCREEN_MIDH:
-                self.bean.jump()
-
-            self.bean.draw(self.screen)
-            pygame.display.update(self.bean.rect)
+            self.update_main_screen()
     
     def play(self):
         lose, score = self.init_variables()
@@ -196,11 +170,45 @@ class FlappyBean:
                 pygame.display.flip()
 
     def init_variables(self):
-        if not self.channel_music.get_busy():
-            self.channel_music.play(self.music_game, -1)
+        self.play_theme()
 
         self.bean.topleft = BEAN_PSCW, BEAN_PSCH
         self.pipes.clear()
         self.pipes.append(Pipe(PIPE_X))
 
         return False, 0
+    
+    def init_main_screen(self):
+        self.bean.topleft = BEAN_MSCW, BEAN_MSCH
+    
+    def display_main_screen(self):
+        self.screen.blit(self.background, (0, 0))
+
+        self.screen.blit(self.flappy, self.flappy_rect)
+        self.screen.blit(self.finger, self.finger_rect)
+        self.screen.blit(self.tap_left, self.tap_left_rect)
+        self.screen.blit(self.tap_right, self.tap_right_rect)
+        self.screen.blit(self.get_ready, self.get_ready_rect)
+        self.screen.blit(self.share, self.share_rect)
+
+        pygame.display.flip()
+
+    def update_main_screen(self):
+        # Clearing current bean position
+        self.screen.blit(self.background,
+                         self.bean.topleft,
+                         area=self.bean.rect)
+        pygame.display.update(self.bean.rect)
+        
+        # Moving the bean
+        if self.bean.y >= SCREEN_MIDH:
+            self.bean.jump()
+        self.bean.move()
+
+        # Drawing the bean in the new position
+        self.bean.draw(self.screen)
+        pygame.display.update(self.bean.rect)
+    
+    def play_theme(self):
+        if not self.channel_music.get_busy():
+            self.channel_music.play(self.music_game, -1)
