d6a39584036f7b95abdf1f90cdb624b37e41d003
filipemedeiross <filipemedeiros2013@gmail.com>
2024-01-11 09:16:19 -0300

    Updated the constants of the game's classes

--- flappy_bean/constants.py
+++ flappy_bean/constants.py
@@ -1,5 +1,11 @@
 # Defining the constants
 
+# Settings
+FRAMERATE_MS = 25
+FRAMERATE_PS = 30
+
+SPEED_ANIMATION = 5
+
 # Dimensions
 SCREEN_WDTH = 500
 SCREEN_HGHT = 700
@@ -10,6 +16,11 @@ SCREEN_SIZE = SCREEN_WDTH, SCREEN_HGHT
 # Dimensions of game elements
 BEAN_MSCW = SCREEN_MIDW - 25
 BEAN_MSCH = SCREEN_MIDH - 60
+BEAN_PSCW = 200
+BEAN_PSCH = 350
+
+PIPE_X  = 700
+PIPE_NX = 600
 
 FLAPPY_SIZE   = 350, 80
 FINGER_SIZE   = 25, 25
@@ -17,13 +28,45 @@ TAP_SIZE      = 40, 25
 GETREADY_SIZE = 200, 50
 SHARE_SIZE    = 60, 40
 
+GAMEOVER_SIZE = 300, 80
+MENU_SIZE     = 100, 50
+OK_SIZE       = 100, 50
+
 DIGITS_SIDE = 20
 DIGITS_SIZE = DIGITS_SIDE, DIGITS_SIDE
 
 # Indent
-FLAPPY_TOP = 100
-FINGER_TOP = 450
+FLAPPY_TOP   = 100
+FINGER_TOP   = 450
+GAMEOVER_TOP = 320
 SPACING = 10
 
 SCORE_LEFT = 20
 SCORE_TOP  = 20
+
+# Constants of the game's classes
+BEAN_SPEED = -10.5
+BEAN_ACCELERATION = 1.5
+BEAN_MAX_DELTA = 16
+BEAN_SPD_ROTATION = 15
+BEAN_MAX_ROTATION = 30
+BEAN_FLY_ROTATION = -70
+BEAN_MIN_ROTATION = -90
+BEAN_WDTH = 50
+BEAN_HGHT = 40
+BEAN_PATHS = ('flappy_bean/media/bean1.png',
+              'flappy_bean/media/bean2.png',
+              'flappy_bean/media/bean3.png',
+              'flappy_bean/media/bean2.png')
+
+BASE_WDTH = SCREEN_WDTH
+BASE_HGHT = 80
+BASE_PATH = 'flappy_bean/media/base.png'
+
+PIPE_DIST = 200
+PIPE_MIN_HGHT = 0
+PIPE_MAX_HGHT = SCREEN_HGHT - (BASE_HGHT + PIPE_DIST)
+PIPE_WDTH = 100
+PIPE_HGHT = PIPE_MAX_HGHT
+PIPE_PATH_TOP  = 'flappy_bean/media/pipe_top.png'
+PIPE_PATH_BASE = 'flappy_bean/media/pipe_base.png'
