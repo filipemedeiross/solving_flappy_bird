18950be6dc1e0974cd1eca3c5d08e76364f2920a
filipemedeiross <filipemedeiros2013@gmail.com>
2024-01-12 10:36:49 -0300

    Adding reading command-line arguments

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from argparse import ArgumentParser
 from flappy_bean import FlappyBean
 
 
+# Getting the command line arguments
+parser = ArgumentParser(description='Game options')
+parser.add_argument('--data-path')
+args = parser.parse_args()
+
 # Testing the game
-game = FlappyBean()
+game = FlappyBean(data_path=args.data_path)
 game.init_game()
