f4646fafe795df149f4410c02660ee29751d094f
filipemedeiross <filipemedeiros2013@gmail.com>
2024-05-20 18:07:55 -0300

    Improving the interface with the "pipe" class

--- flappy_bean/pipe.py
+++ flappy_bean/pipe.py
@@ -2,9 +2,9 @@ from random import randrange
 from pygame.image import load
 from pygame.transform import scale
 from pygame.mask import from_surface
-from .constants import SPEED_ANIMATION, PIPE_DIST, \
+from .constants import SPEED_ANIMATION, PIPE_DIST,   \
                        PIPE_MIN_HGHT, PIPE_MAX_HGHT, \
-                       PIPE_WDTH, PIPE_HGHT, \
+                       PIPE_WDTH, PIPE_HGHT,         \
                        PIPE_PATH_TOP, PIPE_PATH_BASE
 
 
@@ -24,40 +24,44 @@ class Pipe:
         self.x = x
         self.passed = False
 
-        self.pipe_top  = scale(load(self.PATH_TOP), self.SIZE)
-        self.pipe_base = scale(load(self.PATH_BASE), self.SIZE)
-        self.mask_top  = from_surface(self.pipe_top)
-        self.mask_base = from_surface(self.pipe_base)
-
         self.set_height()
 
+        self.pipe_top  = self.load_image(self.PATH_TOP , self.SIZE)
+        self.pipe_base = self.load_image(self.PATH_BASE, self.SIZE)
+        self.mask_top  = from_surface(self.pipe_top )
+        self.mask_base = from_surface(self.pipe_base)
+
     def set_height(self):
-        self.height = randrange(self.MIN_HGHT, self.MAX_HGHT)
-        self.pos_top  = self.height - self.HGHT
-        self.pos_base = self.height + self.DIST
+        height = randrange(self.MIN_HGHT, self.MAX_HGHT + 1)
+        self.pos_top  = height - self.HGHT
+        self.pos_base = height + self.DIST
 
     def move(self):
         self.x -= self.SPEED
 
-    def draw(self, screen):
-        screen.blit(self.pipe_top, (self.x, self.pos_top))
-        screen.blit(self.pipe_base, (self.x, self.pos_base))
+    def draw(self, surface):
+        surface.blit(self.pipe_top , (self.x, self.pos_top ))
+        surface.blit(self.pipe_base, (self.x, self.pos_base))
 
     def collide(self, obj):
         mask = obj.mask
 
-        dist_top  = (self.x - obj.x, self.pos_top  - round(obj.y))
-        dist_base = (self.x - obj.x, self.pos_base - round(obj.y))
+        dist_top  = self.x - obj.x, self.pos_top  - round(obj.y)
+        dist_base = self.x - obj.x, self.pos_base - round(obj.y)
 
-        overlap_top  = mask.overlap(self.mask_top, dist_top)
+        overlap_top  = mask.overlap(self.mask_top , dist_top )
         overlap_base = mask.overlap(self.mask_base, dist_base)
 
         return overlap_top or overlap_base
 
     @property
     def right(self):
-        return self.x + self.pipe_top.get_width()
+        return self.x + self.WDTH
 
     @property
     def center(self):
         return self.x + (self.WDTH / 2), self.pos_base - (self.DIST / 2)
+
+    @staticmethod
+    def load_image(path, size):
+        return scale(load(path), size)
